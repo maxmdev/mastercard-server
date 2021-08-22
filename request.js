f033f53a504cc2e012658c47a9ae79961d0c606b
Maxim <reach.wire@pm.me>
2021-08-23 00:02:15 +0300

    added error handler

--- request.js
+++ request.js
@@ -2,11 +2,18 @@ const fetch = require("node-fetch");
 
 async function request(uri, params = {}) {
         const response = await fetch(uri, params);
+        const responseData = await response.json();
 
         if (response.status === 200) {
-            return await response.json();
+            return responseData;
         } else {
-            throw new Error('An error occurred.' + response.status);
+            const error = JSON.parse(JSON.stringify(responseData)).Errors.Error[0].Description || false;
+
+            if (error) {
+                throw new Error(response.status + ': ' + error + '.')
+            } else {
+                return responseData;
+            }
         }
 }
 
