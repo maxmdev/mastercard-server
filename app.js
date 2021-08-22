5c8abb03217a9204c0283fea76e7755c2dd9217c
Maxim <reach.wire@pm.me>
2021-08-22 14:23:55 +0300

    added and tested endpoint for retrieving inquiry history data

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Defines constants
 const API_SANDBOX_URL = 'https://sandbox.api.mastercard.com/fraud/merchant/v3/';
-//const API_PROD_URL = 'https://api.mastercard.com/fraud/merchant/v3/';
+const API_PROD_URL = 'https://api.mastercard.com/fraud/merchant/v3/';
 const API_URL = API_SANDBOX_URL;
 
 // Imports libraries
@@ -20,7 +20,7 @@ server.use(express.json());
 server.use(formData.parse())
 
 // GET *
-server.get('*', (req, res) => {
+server.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
 });
 
@@ -32,9 +32,11 @@ server.post('/api/termination-inquiry', (req, res) => {
     // Defines signing key variable
     const signingKey = keyRetriever.retrieveKey(data.privateKey.path, data.keyPassword, data.keyAlias);
 
-    // Defines OAuth Authorization Header
-    const uri = API_URL+req.url.toString().split('/api/').pop()
+    // Defines request parameters
+    const uri = API_URL+req.url.toString().split('/api/').pop();
     const method = 'POST';
+
+    // Defines OAuth Authorization Header
     const authHeader = oauthSigner.getAuthorizationHeader(uri, method, data.bodyData, data.consumerKey, signingKey);
 
     // Performs a request to MasterCard
@@ -53,11 +55,46 @@ server.post('/api/termination-inquiry', (req, res) => {
             res.status(200).json(data);
         })
         .catch(error => {
-            res.status(200).json(error);
+            res.status(400).json(error);
         })
 })
 
-// GET /termination-inquiry/{id}
+// GET /termination-inquiry/{IRN}
+server.post('/api/termination-inquiry/:IRN', (req, res) => {
+    // Retrieves data from request
+    const data = perform.retrieve(req);
+
+    // Defines a signing key variable
+    const signingKey = keyRetriever.retrieveKey(data.privateKey.path, data.keyPassword, data.keyAlias);
+
+    // Defines request parameters
+    const uri = API_URL + req.url.toString().split('/api/').pop();
+    console.log(uri);
+    const method = 'GET';
+
+    // Defines OAuth Authorization Header
+    const payload = null;
+    const authHeader = oauthSigner.getAuthorizationHeader(uri, method, payload, data.consumerKey, signingKey);
+
+    // Performs a request to MasterCard
+    perform.request(uri, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': authHeader
+        },
+        body: null,
+        cache: 'no-cache',
+        redirect: 'follow'
+    })
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(error => {
+            res.status(400).json(error);
+        })
+})
 
 
 server.listen(3000, () => console.log('Started successfully on port 3000...'));
