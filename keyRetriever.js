3a5a026f79e3275303784b14429e6e4586862f6a
Maxim <reach.wire@pm.me>
2021-08-23 21:55:51 +0300

    added log to p12 var

--- keyRetriever.js
+++ keyRetriever.js
@@ -4,10 +4,9 @@ const fs = require('fs');
 function retrieveKey(filePath, keyPassword, keyAlias) {
     // Retrieves signing key from a certificate
     const p12Content = fs.readFileSync(filePath, 'binary');
-    console.log('p12 content: ', p12Content);
     const p12Asn1 = forge.asn1.fromDer(p12Content);
-    console.log(p12Asn1);
     const p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, false, keyPassword);
+    console.log('p12:', p12);
     const keyObj = p12.getBags({
         friendlyName: keyAlias,
         bagType: forge.pki.oids.pkcs8ShroudedKeyBag
