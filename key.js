2bf671e5720e7137feb280089346f097214aa483
Maxim <reach.wire@pm.me>
2021-08-23 20:04:58 +0300

    added console log to path do dbg

--- key.js
+++ key.js
@@ -2,6 +2,7 @@ const keyRetriever = require('./keyRetriever.js');
 const fs = require('fs');
 
 function get(data) {
+    console.log(data.privateKey);
     return keyRetriever.retrieveKey(data.privateKey.path, data.keyPassword, data.keyAlias);
 }
 
