1e9a041026ea990e05b38ec0d7bdfc0e5cfc82ce
Maxim <reach.wire@pm.me>
2021-08-23 20:35:32 +0300

    changed file type in key retriever

--- keyRetriever.js
+++ keyRetriever.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 function retrieveKey(filePath, keyPassword, keyAlias) {
     // Retrieves signing key from a certificate
-    const p12Content = fs.readFileSync(filePath, 'binary');
+    const p12Content = fs.readFileSync(filePath, 'utf-8');
     const p12Asn1 = forge.asn1.fromDer(p12Content, false);
     const p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, false, keyPassword);
     const keyObj = p12.getBags({
