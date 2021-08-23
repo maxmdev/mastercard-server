cf0c6d0bd6636125321f81851a67900009080394
Maxim <reach.wire@pm.me>
2021-08-23 19:24:21 +0300

    fix file path

--- key.js
+++ key.js
@@ -2,7 +2,7 @@ const keyRetriever = require('./keyRetriever.js');
 const fs = require('fs');
 
 function get(data) {
-    return keyRetriever.retrieveKey(data.privateKey, data.keyPassword, data.keyAlias);
+    return keyRetriever.retrieveKey(data.privateKey.path, data.keyPassword, data.keyAlias);
 }
 
 module.exports = {get}
