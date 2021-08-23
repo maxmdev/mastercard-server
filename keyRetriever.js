3cff25099da96a3762f9c7fdc19b70fa182b9979
Maxim <reach.wire@pm.me>
2021-08-23 20:50:36 +0300

    added console for p12 var

--- keyRetriever.js
+++ keyRetriever.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 
 function retrieveKey(filePath, keyPassword, keyAlias) {
     // Retrieves signing key from a certificate
-    const p12Content = fs.readFileSync(filePath, 'utf-8');
+    const p12Content = fs.readFileSync(filePath, 'binary');
+    console.log('p12 content: ', p12Content);
     const p12Asn1 = forge.asn1.fromDer(p12Content, false);
     const p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, false, keyPassword);
     const keyObj = p12.getBags({
