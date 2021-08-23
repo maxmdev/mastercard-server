cba1a9b60a1610ee2d39ea956d0d8b30be5ff303
Maxim <reach.wire@pm.me>
2021-08-23 19:18:05 +0300

    remove errors with file cert receiving

--- key.js
+++ key.js
@@ -1,7 +1,8 @@
 const keyRetriever = require('./keyRetriever.js');
+const fs = require('fs');
 
 function get(data) {
-    return keyRetriever.retrieveKey(data.privateKey.path || data.privateKey, data.keyPassword, data.keyAlias);
+    return keyRetriever.retrieveKey(data.privateKey, data.keyPassword, data.keyAlias);
 }
 
 module.exports = {get}
