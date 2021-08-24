9b36601e719e52c6c2c183b1ce57c8c34fab4c2b
Maxim <reach.wire@pm.me>
2021-08-24 16:24:51 +0300

    added method to use private key only

--- key.js
+++ key.js
@@ -2,8 +2,11 @@ const keyRetriever = require('./keyRetriever.js');
 const fs = require('fs');
 
 function get(data) {
-    //return keyRetriever.retrieveKey(data.privateKey.path, data.keyPassword, data.keyAlias);
+    return keyRetriever.retrieveKey(data.privateKey.path, data.keyPassword, data.keyAlias);
+}
+
+function getPrivate(data) {
     return keyRetriever.sendPrivateKey(data.privateKey.path);
 }
 
-module.exports = {get}
+module.exports = {get, getPrivate}
