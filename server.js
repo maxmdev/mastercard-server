692744ebcc690145c1e1ca46645102b5b048ab87
Maxim <reach.wire@pm.me>
2021-08-26 01:42:19 +0300

    added Reference ID to Inquiry History Request endpoint output

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ server.post('/api/termination-inquiry/:IRN', (req, res) => {
         cache: 'no-cache',
         redirect: 'follow'
     })
-        .then(data => res.status(200).json(data))
+        .then(data => res.status(200).json(postprocess.addReferenceID(data)))
         .catch(error => res.status(500).send(error.message))
 });
 
